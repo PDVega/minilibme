abbc1948a57c73558d1e0d91340fa81533e811a3
pdvega <pdvegaa@gmail.com>
2017-07-20 21:06:48 +0700

    add layout

--- routes/index.js
+++ routes/index.js
@@ -18,32 +18,18 @@ router.get('/login', function(req,res){
 })
 
 router.post('/login', (req,res) => {
-  // if(!req.body.username || !req.body.password)
-  if(!req.body.username)
-  {
-    res.send('Please enter username and password')
-  }
-  else
-  {
-    db.User.findOne({
-      where: {
-        username:req.body.username
-        // username:req.body.username
-      }
+  db.User.findOne({
+    where: { username:req.body.username }
     })
     .then(function(data_user){
       const secret = data_user.salt;
       const hashData = hash(secret, req.body.password)
       // console.log('hashData',hashData);
       // console.log('datauser',data_user.password);
-      if(hashData == data_user.password)
-      {
+      if(hashData == data_user.password){
       // console.log(data_user.username);
-          req.session.user = {
-            username: data_user.username
-          }
-      }else
-      {
+        req.session.user = { username: data_user.username }
+      } else {
           res.send('Wrong Password')
       }
       res.redirect('/books')
@@ -51,7 +37,6 @@ router.post('/login', (req,res) => {
     .catch(function(err){
       res.send('User Not Found')
     })
-  }
 })
 
 router.get('/logout', function(req,res){
